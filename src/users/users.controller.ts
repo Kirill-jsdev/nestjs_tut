fe3d6a230099c2bdfd0d81824f0fb08a3d56b19a
Kirill Zlobin <jsdevzk@gmail.com>
2025-09-01 17:38:32 +0200

    Created a patch user DTO

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,27 +5,40 @@ import {
   Get,
   Param,
   ParseIntPipe,
+  Patch,
   Post,
   Query,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { GetUsersParamDto } from './dtos/get-users-param.dto';
+import { PatchUserDto } from './dtos/patch-user.dto';
 
 @Controller('users')
 export class UsersController {
-  @Get('/:id')
+  @Get('{/:id}')
   public getUsers(
-    @Param('id', ParseIntPipe) id: number,
+    @Param() params: GetUsersParamDto,
     @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
     @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
   ) {
     console.log('Limit:', limit);
     console.log('Page:', page);
-    return 'This action returns all users with id ' + id + ` ${limit} ${page}`;
+    return (
+      'This action returns all users with id ' +
+      params?.id +
+      ` ${limit} ${page}`
+    );
   }
 
   @Post()
   public createUser(@Body() createUserDto: CreateUserDto) {
     console.log('Body:', createUserDto);
     return 'This action adds a new user';
+  }
+
+  @Patch()
+  public patchUser(@Body() patchUserDto: PatchUserDto) {
+    console.log('Body:', patchUserDto);
+    return 'This action updates a user';
   }
 }
