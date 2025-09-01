35d5c46ceee30711d155bc9719625f1f0efcc751
Kirill Zlobin <jsdevzk@gmail.com>
2025-09-01 16:09:03 +0200

    Used validation pipes for URL params and queries

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,25 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  DefaultValuePipe,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
   @Get('/:id')
-  public getUsers(@Param() params: any, @Query() query: any) {
-    console.log('Query:', query);
-    return 'This action returns all users with id ' + params.id;
+  public getUsers(
+    @Param('id', ParseIntPipe) id: number,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+  ) {
+    console.log('Limit:', limit);
+    console.log('Page:', page);
+    return 'This action returns all users with id ' + id + ` ${limit} ${page}`;
   }
 
   @Post()
