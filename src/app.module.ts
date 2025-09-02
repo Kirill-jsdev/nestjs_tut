06a2e3979a6b7381c0c95cecbc7c945e8085be8b
Kirill Zlobin <jsdevzk@gmail.com>
2025-09-03 00:54:53 +0200

    Connected app to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,24 @@ import { AppService } from './app.service';
 import { UsersModule } from 'src/users/users.module';
 import { PostsModule } from './posts/posts.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, PostsModule, AuthModule],
+  imports: [
+    UsersModule,
+    PostsModule,
+    AuthModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'nest',
+      password: 'nest',
+      database: 'nestdb',
+      entities: [],
+      synchronize: true, // Note: set to false in production
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
