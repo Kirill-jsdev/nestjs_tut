7127cc182f4cfc015b792dcc0e10567e24fb5063
Kirill Zlobin <jsdevzk@gmail.com>
2025-09-02 17:16:20 +0200

    Added validation to CreatePostDTO with class-validator library

--- src/posts/dtos/create-post.dto.ts
+++ src/posts/dtos/create-post.dto.ts
@@ -1,15 +1,62 @@
+import {
+  IsArray,
+  IsEnum,
+  IsISO8601,
+  IsJSON,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsUrl,
+  Matches,
+  MinLength,
+} from 'class-validator';
 import { PostStatusEnum } from '../enums/post-stutus.enum';
 import { PostTypeEnum } from '../enums/post-type.enum';
 
 export class CreatePostDto {
+  @IsString()
+  @MinLength(4)
+  @IsNotEmpty()
   title: string;
+
+  @IsEnum(PostTypeEnum)
+  @IsNotEmpty()
   postType: PostTypeEnum;
+
+  @IsString()
+  @IsNotEmpty()
+  @Matches(/^([a-z0-9]+)(?:-[a-z0-9]+)*$/, {
+    message:
+      'A slug should be all small letters and uses only "-" and without spaces. For example "my-url"',
+  })
   slug: string;
+
+  @IsEnum(PostStatusEnum)
+  @IsNotEmpty()
   status: PostStatusEnum;
+
+  @IsString()
+  @IsOptional()
   content?: string;
+
+  @IsOptional()
+  @IsJSON()
   schema?: string;
+
+  @IsOptional()
+  @IsUrl()
   featuredImageUrl?: string;
+
+  @IsISO8601()
+  @IsOptional()
   publishedOn: Date;
+
+  @IsOptional()
+  @IsArray()
+  @IsString({ each: true })
+  @MinLength(3, { each: true })
   tags?: string[];
+
+  @IsOptional()
   metaOptions?: [{ key: 'sidebar'; value: true }];
 }
