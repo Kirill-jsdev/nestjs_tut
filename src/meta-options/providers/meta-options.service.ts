7a3114ef19616191e63e4fe020bfe9697c8e229b
Kirill Zlobin <jsdevzk@gmail.com>
2025-09-03 17:32:36 +0200

    Created MetaOptionsService

--- src/meta-options/providers/meta-options.service.ts
+++ src/meta-options/providers/meta-options.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { CreatePostMetaOptionsDto } from '../dtos/create-post-meta-options.dto';
+import { MetaOption } from '../meta-option.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class MetaOptionsService {}
+export class MetaOptionsService {
+  constructor(
+    /**  Inject the MetaOption repository */
+    @InjectRepository(MetaOption)
+    private readonly metaOptionRepository: Repository<MetaOption>,
+  ) {}
+
+  public async create(createPostMetaOptionsDto: CreatePostMetaOptionsDto) {
+    const metaOption = this.metaOptionRepository.create(
+      createPostMetaOptionsDto,
+    );
+    return this.metaOptionRepository.save(metaOption);
+  }
+}
