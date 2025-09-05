6d194fe1cb99dd86e7c8b1dcc989a155c68a0ba9
Kirill Zlobin <jsdevzk@gmail.com>
2025-09-05 17:09:07 +0200

    FIRST TRANSACTION is implemented for creating multiple users with all steps description

--- src/users/providers/users.service.ts
+++ src/users/providers/users.service.ts
@@ -10,7 +10,7 @@ import {
 } from '@nestjs/common';
 import { AuthService } from 'src/auth/providers/auth.service';
 import { User } from '../user.entity';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateUserDto } from '../dtos/create-user.dto';
 import profileConfig from '../config/profile.config';
@@ -33,6 +33,8 @@ export class UsersService {
 
     @Inject(profileConfig.KEY)
     private readonly profileConfiguration: ConfigType<typeof profileConfig>,
+
+    private readonly dataSource: DataSource,
   ) {}
 
   public async createUser(createUserDto: CreateUserDto): Promise<User> {
@@ -116,5 +118,37 @@ export class UsersService {
     }
 
     return user;
+  }
+
+  //this method will create multiple users in a transaction. WE IMPLEMENT A TRANSACTION
+  public async createManyUsers(createUserDtos: CreateUserDto[]) {
+    const newUsers: User[] = [];
+    //create a query runner
+    const queryRunner = this.dataSource.createQueryRunner();
+
+    //connect query runner instance to the database
+    await queryRunner.connect();
+
+    //start a transaction
+    await queryRunner.startTransaction();
+
+    try {
+      for (const createUserDto of createUserDtos) {
+        const user = queryRunner.manager.create(User, createUserDto);
+        const result = await queryRunner.manager.save(user);
+        newUsers.push(result);
+      }
+      //if successful, commit the transaction
+      await queryRunner.commitTransaction();
+    } catch (error) {
+      console.error('Database error:', error);
+      //if error, rollback the transaction
+      await queryRunner.rollbackTransaction();
+    } finally {
+      //release connection
+      await queryRunner.release();
+    }
+    //return created users
+    return newUsers;
   }
 }
