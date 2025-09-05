5eb707a1a4aa38176312c5a3fceb6fda1d03ce8d
Kirill Zlobin <jsdevzk@gmail.com>
2025-09-05 12:21:42 +0200

    Used app config function which is used by ConfigModule provided by Nest JS to set up env variables for DB configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TagsModule } from './tags/tags.module';
 import { MetaOptionsModule } from './meta-options/meta-options.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { appConfig } from './config/app.config';
 
 const ENV = process.env.NODE_ENV;
 
@@ -19,20 +20,21 @@ const ENV = process.env.NODE_ENV;
     ConfigModule.forRoot({
       isGlobal: true, //makes the module available globally without needing to import it in other modules
       envFilePath: !ENV ? '.env' : `.env.${ENV}`, //dynamically set env file based on the environment
+      load: [appConfig], //we can pass configuration functions here
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
-        host: configService.get('DATABASE_HOST'),
-        port: +configService.get('DATABASE_PORT'), //the plus sign converts string to number
-        username: configService.get('DATABASE_USER'),
-        password: configService.get('DATABASE_PASSWORD'),
-        database: configService.get('DATABASE_NAME'),
+        host: configService.get('database.host'),
+        port: configService.get('database.port'),
+        username: configService.get('database.username'),
+        password: configService.get('database.password'),
+        database: configService.get('database.name'),
         // entities: [User], //we do not need this as we are using autoLoadEntities
-        autoLoadEntities: true,
-        synchronize: true, // Note: set to false in production. This way during development we will not need to use migrations
+        autoLoadEntities: configService.get('database.autoLoadEntities'), //on production always should be false
+        synchronize: configService.get('database.synchronize'), //on production always should be false
       }),
     }),
     TagsModule,
---
+++ src/config/app.config.ts
@@ -0,0 +1,14 @@
+export const appConfig = () => ({
+  environment: process.env.NODE_ENV || 'production',
+  database: {
+    host: process.env.DATABASE_HOST || 'localhost',
+    port: +(process.env.DATABASE_PORT || 0) || 5432,
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    name: process.env.DATABASE_NAME,
+
+    //nest js specific variables
+    synchronize: process.env.DATABASE_SYNC === 'true' ? true : false,
+    autoLoadEntities: process.env.AUTO_LOAD_ENTITIES === 'true' ? true : false,
+  },
+});
