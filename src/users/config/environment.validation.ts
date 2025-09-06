ae632f6236e38606f45cbc67c563d96a84d8d72d
Kirill Zlobin <jsdevzk@gmail.com>
2025-09-06 17:02:46 +0200

    Added validation for JWT environment variables. Validation of env variables will not allow to start an app if some required ENV variables are missing

--- src/users/config/environment.validation.ts
+++ src/users/config/environment.validation.ts
@@ -12,4 +12,9 @@ export default Joi.object({
   DATABASE_SYNC: Joi.boolean().default(false),
   AUTO_LOAD_ENTITIES: Joi.boolean().default(false),
   PROFILE_API_KEY: Joi.string().required(),
+
+  JWT_SECRET: Joi.string().required(),
+  JWT_TOKEN_AUDIENCE: Joi.string().required(),
+  JWT_TOKEN_ISSUER: Joi.string().required(),
+  JWT_ACCESS_TOKEN_TTL: Joi.number().default(3600),
 });
