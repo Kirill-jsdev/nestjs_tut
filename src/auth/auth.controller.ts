3bba0f3a7e4e6e753648631eb8e445ce8f2e7b64
Kirill Zlobin <jsdevzk@gmail.com>
2025-09-06 23:01:45 +0200

    Implemented AuthenticationGuard. Removed eslint file

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,8 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './providers/auth.service';
 import { SignInDto } from './providers/signin.dto';
+import { Auth } from './decorators/auth.decorator';
+import { AuthTypeEnum } from './enums/auth-type.enum';
 
 @Controller('auth')
 export class AuthController {
@@ -8,6 +10,7 @@ export class AuthController {
 
   @Post('sign-in')
   @HttpCode(HttpStatus.OK)
+  @Auth(AuthTypeEnum.NONE)
   public async signIn(@Body() signInDto: SignInDto) {
     //signIn logic here
     return await this.authService.signIn(signInDto);
--- src/auth/guards/authentication/authentication.guard.ts
+++ src/auth/guards/authentication/authentication.guard.ts
@@ -1,16 +1,14 @@
-import { type CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { type CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Observable } from 'rxjs';
 import { AccessTokenGuard } from '../access-token/access-token.guard';
 import { AuthTypeEnum } from 'src/auth/enums/auth-type.enum';
+import { AUTH_TYPE_KEY } from 'src/auth/constants/auth.constants';
 
 @Injectable()
 export class AuthenticationGuard implements CanActivate {
   private static readonly defaultAuthType = AuthTypeEnum.BEARER;
-  private readonly authTypeGuardMap: Record<
-    AuthTypeEnum,
-    CanActivate | CanActivate[]
-  >;
+  private readonly authTypeGuardMap: Record<AuthTypeEnum, CanActivate | CanActivate[]>;
 
   constructor(
     private readonly reflector: Reflector,
@@ -22,10 +20,29 @@ export class AuthenticationGuard implements CanActivate {
     };
   }
 
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    console.log(this.authTypeGuardMap);
-    return true;
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const authTypes = this.reflector.getAllAndOverride<AuthTypeEnum[]>(AUTH_TYPE_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]) ?? [AuthenticationGuard.defaultAuthType];
+
+    console.log('Auth Types:', authTypes);
+
+    const guards = authTypes.map((type) => this.authTypeGuardMap[type]).flat();
+    console.log('Guards to execute:', guards);
+
+    const error = new UnauthorizedException('Unauthorized');
+
+    for (const guard of guards) {
+      const canActivate = await Promise.resolve(guard.canActivate(context)).catch((err) => {
+        error: err;
+      });
+
+      if (canActivate) {
+        return true;
+      }
+    }
+
+    throw error;
   }
 }
