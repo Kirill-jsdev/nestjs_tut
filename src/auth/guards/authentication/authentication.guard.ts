80dc8829106276b9e63520f07c7b629290ad0de9
Kirill Zlobin <jsdevzk@gmail.com>
2025-09-06 22:01:39 +0200

    Added defaultAuthType  (vid 157, 5:10)

--- src/auth/guards/authentication/authentication.guard.ts
+++ src/auth/guards/authentication/authentication.guard.ts
@@ -2,9 +2,11 @@ import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Observable } from 'rxjs';
 import { AccessTokenGuard } from '../access-token/access-token.guard';
+import { AuthTypeEnum } from 'src/auth/enums/auth-type.enum';
 
 @Injectable()
 export class AuthenticationGuard implements CanActivate {
+  private static readonly defaultAuthType = AuthTypeEnum.BEARER;
   constructor(
     private readonly reflector: Reflector,
     private readonly accessTokenGuard: AccessTokenGuard,
