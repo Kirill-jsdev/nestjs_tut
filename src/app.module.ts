96375cd9d11c66f8e6d8b48ddd7d1501b3f53a16
Kirill Zlobin <jsdevzk@gmail.com>
2025-09-06 16:56:56 +0200

    Protected the whole app. If any module provides the stuff that is provided in this commit - the whole app will be protected. Not just a certain module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,6 +12,10 @@ import { PaginationModule } from './common/pagination/pagination.module';
 import appConfig from './config/app.config';
 import databaseConfig from './config/database.config';
 import environmentValidation from './users/config/environment.validation';
+import jwtConfig from './auth/config/jwt.config';
+import { JwtModule } from '@nestjs/jwt';
+import { APP_GUARD } from '@nestjs/core';
+import { AccessTokenGuard } from './auth/guards/access-token/access-token.guard';
 
 const ENV = process.env.NODE_ENV;
 
@@ -44,8 +48,18 @@ const ENV = process.env.NODE_ENV;
     TagsModule,
     MetaOptionsModule,
     PaginationModule,
+
+    ConfigModule.forFeature(jwtConfig),
+    JwtModule.registerAsync(jwtConfig.asProvider()),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      //this is the way to protect the entire module with a guard. If it was in a specific controller, it would protect only that controller
+      provide: APP_GUARD,
+      useClass: AccessTokenGuard,
+    },
+  ],
 })
 export class AppModule {}
